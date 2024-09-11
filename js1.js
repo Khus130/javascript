0961770c80a0b26afa6a6336b417653023b09532
Khus130 <ishanvisingh75@gmail.com>
2024-09-11 11:07:46 +0530

    intial

---
+++ js1.js
@@ -0,0 +1,20 @@
+let colors = ["red","blue"];
+colors[1] = "yellow";
+console.log(colors);
+
+console.log(Object.entries(person))
+
+array
+
+function sum(a,b)
+{
+    return a+b;
+}
+sum(9,8);
+
+input1 = 
+
+input1= parseInt(input1);
+input2 = parseInt(input2)
+
+function 
